c19037d75327d5ace7a46197309e1f92bdbbc9cc
Daniel Araújo <62966905+DanielNNF@users.noreply.github.com>
2020-10-05 07:07:52 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, AsyncStorage } from 'react-native';
 
 export default function App() {
+  const[nome, setNome] =useState();
+  const save = async() => {
+    try {
+      await AsyncStorage.setItem("MeuNome", nome);
+    } catch (error) {
+      alert(error)
+    }
+
+  };
+
+  const load = async () => {
+    try {
+      let nome = await AsyncStorage.getItem("MeuNome");
+
+      if(nome !== null){
+        setNome(nome)
+      }
+    } catch (error) {
+      alert (error);
+    }
+  };
+
+  const remove = async () => {
+    try {
+      await AsyncStorage.removeItem("MeuNome")
+    } catch (error) {
+      alert(error)
+    }finally{
+      setNome("");
+
+    }
+
+  };
+
+
+
+
+  useEffect(() =>{
+    load();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source ={require("./assets/images.png")}style={{width:"100%", height:200,marginTop:64}} resizeMode="contain"/>
+      
+  <Text style={{height:30}}>{nome}</Text> 
+      
+      <Text style ={styles.name}>Qual seu nome?</Text>
+      
+      <TextInput style={styles.input} onChangeText = {(text) => setNome(text)}/> 
+
+      <TouchableOpacity style ={styles.button} onPress={() => save()}>
+        <Text style ={{color:"#fff"}}>Guarde meu nome!</Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity style ={styles.button} onPress={() => remove()}>
+        <Text style ={{color:"#fff"}}>Apagar meu nome!</Text>
+      </TouchableOpacity>
+      
     </View>
   );
 }
@@ -16,6 +71,35 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  name:{
+    fontSize:24,
+    fontWeight:"300",
+  },
+  input:{
+    borderWidth:1,
+    borderColor:"#575dd9",
+    alignSelf:"stretch",
+    margin:32,
+    height:64,
+    borderRadius: 6,
+    paddingHorizontal:16,
+    fontSize:24,
+
+
+  },
+  button:{
+    backgroundColor:"#575dd9",
+    alignItems: "center",
+    justifyContent:"center",
+    alignSelf: "stretch",
+    paddingVertical:12,
+    paddingHorizontal:32,
+    marginTop:32,
+    marginHorizontal:32,
+    borderRadius:6,
+
+
+  }
 });
